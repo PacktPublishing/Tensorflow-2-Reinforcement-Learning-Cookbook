299233cbd1351c7886a07cd828216f7f1940372e
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-06-20 02:27:15 -0700

    Added state & action defs for a simple Gridworld env

---
+++ src/Ch1-developing-building-blocks-for-deep-rl/envs/gridworld.py
@@ -0,0 +1,66 @@
+import copy
+
+import gym
+import numpy as np
+
+# Grid cell state and color mapping
+EMPTY = BLACK = 0
+WALL = GRAY = 1
+AGENT = BLUE = 2
+MINE = RED = 3
+TARGET = GREEN = 4
+SUCCESS = PINK = 5
+
+# RGB color value table
+COLOR_MAP = {
+    BLACK: [0.0, 0.0, 0.0],
+    GRAY: [0.5, 0.5, 0.5],
+    BLUE: [0.0, 0.0, 1.0],
+    RED: [1.0, 0.0, 0.0],
+    GREEN: [0.0, 1.0, 0.0],
+    PINK: [1.0, 0.0, 1.0],
+}
+
+# Action mapping
+NOOP = 0
+DOWN = 1
+UP = 2
+LEFT = 3
+RIGHT = 4
+
+
+class GridworldEnv(gym.Env):
+    def __init__(self):
+        # Observations
+        self.img_shape = [256, 256, 3]
+        grid_layout = """
+        1 1 1 1 1 1 1 1
+        1 2 0 0 0 0 0 1
+        1 0 1 1 1 0 0 1
+        1 0 1 0 1 0 0 1
+        1 0 1 4 1 0 0 1
+        1 0 3 0 0 0 0 1
+        1 0 0 0 0 0 0 1
+        1 1 1 1 1 1 1 1
+        """
+        self.initial_grid_state = np.fromstring(grid_layout, dtype=int, sep=" ")
+        self.initial_grid_state = self.initial_grid_state.reshape(8, 8)
+        self.grid_state = copy.deepcopy(self.initial_grid_state)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=6, shape=self.grid_state.shape
+        )
+        self.metadata = {"render.modes": ["human"]}
+        # Actions
+        self.action_space = gym.spaces.Discrete(5)
+        self.actions = [NOOP, UP, DOWN, LEFT, RIGHT]
+        self.action_pos_dict = {
+            NOOP: [0, 0],
+            UP: [-1, 0],
+            DOWN: [1, 0],
+            LEFT: [0, -1],
+            RIGHT: [0, 1],
+        }
+        (self.agent_start_state, self.agent_target_state,) = self.get_state()
+
+        self.viewer = None
+
