3a6e066112a11c7585313a8c86623f82a964a14a
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-07-28 00:28:54 -0700

    Added GridworldV2 Env for value function recipes

---
+++ src/ch2-value-based-policy-gradients-and-actor-critic-methods/envs/gridworldv2.py
@@ -0,0 +1,97 @@
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Gridworldv2Env(gym.Env):
+    def __init__(self, step_cost=-0.2, max_ep_length=500, explore_start=False):
+        self.map = np.zeros((3, 4))
+        self.observation_space = gym.spaces.Discrete(1)
+        self.map[0][3] = 1  # goal state
+        self.map[1][3] = -1  # Terminal state
+        self.map[1][1] = 2  # Wall
+        self.index_to_coordinate_map = {
+            "0": [0, 0],
+            "1": [0, 1],
+            "2": [0, 2],
+            "3": [0, 3],
+            "4": [1, 0],
+            "5": [1, 1],
+            "6": [1, 2],
+            "7": [1, 3],
+            "8": [2, 0],
+            "9": [2, 1],
+            "10": [2, 2],
+            "11": [2, 3],
+        }
+        self.coordinate_to_index_map = {
+            str(val): int(key) for key, val in self.index_to_coordinate_map.items()
+        }
+        self.exploring_starts = explore_start
+        self.goal_state = 3
+        self.death_state = 7
+        self.state = 8
+        self.done = False
+        self.max_ep_length = max_ep_length
+        self.steps = 0
+        self.step_cost = step_cost
+        self.action_space = gym.spaces.Discrete(4)
+        self.action_map = {"UP": 0, "RIGHT": 1, "DOWN": 2, "LEFT": 3}
+        self.possible_actions = list(self.action_map.values())
+
+    def reset(self):
+        self.done = False
+        self.steps = 0
+        self.map = np.zeros((3, 4))
+        self.map[0][3] = 1  # goal state
+        self.map[1][3] = -1  # death state
+        self.map[1][1] = 2  # block
+        if self.exploring_starts:
+            self.state = np.random.choice([0, 1, 2, 4, 6, 8, 9, 10, 11])
+        else:
+            self.state = 8
+        return self.state
+
+    def get_next_state(self, current_position, action):
+
+        next_state = self.index_to_coordinate_map[str(current_position)].copy()
+
+        if action == 0 and next_state[0] != 0 and next_state != [2, 1]:
+            # Move up
+            next_state[0] -= 1
+        elif action == 1 and next_state[1] != 3 and next_state != [1, 0]:
+            # Move right
+            next_state[1] += 1
+        elif action == 2 and next_state[0] != 2 and next_state != [0, 1]:
+            # Move down
+            next_state[0] += 1
+        elif action == 3 and next_state[1] != 0 and next_state != [1, 2]:
+            # Move left
+            next_state[1] -= 1
+        else:
+            pass
+        return self.coordinate_to_index_map[str(next_state)]
+
+    def step(self, action):
+        assert action in self.possible_actions, f"Invalid action:{action}"
+
+        current_position = self.state
+        next_state = self.get_next_state(current_position, action)
+
+        self.steps += 1
+
+        if next_state == self.goal_state:
+            reward = 1
+            self.done = True
+
+        elif next_state == self.death_state:
+            reward = -1
+            self.done = True
+        else:
+            reward = self.step_cost
+
+        if self.steps == self.max_ep_length:
+            self.done = True
+
+        self.state = next_state
+        return next_state, reward, self.done
