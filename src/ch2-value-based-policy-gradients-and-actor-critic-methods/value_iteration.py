904e1fa354e8e70f79a1db4129b5971979faa84e
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-07-27 17:18:36 -0700

    Updated value iteration

--- src/ch2-value-based-policy-gradients-and-actor-critic-methods/value_iteration.py
+++ src/ch2-value-based-policy-gradients-and-actor-critic-methods/value_iteration.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from maze import MazeEnv
+from envs.maze import MazeEnv
 from value_function_plot import value_function_plot
 
 discount = 0.9
@@ -9,12 +9,12 @@ iters = 1000
 env = MazeEnv()
 state_dim = env.distinct_states
 state_values = np.zeros(state_dim)
-Qval = np.zeros((state_dim, env.action_space.n))  # Action values
+q_values = np.zeros((state_dim, env.action_space.n))  # Action values
 policy = np.zeros(state_dim)
 
 
-def calcQ(state, action):
-    """Calculate Value function for given state and action
+def calculate_values(state, action):
+    """Evaluate Value function for given state and action
 
     Args:
         state (int): Valid (discrete) state in discrete `env.observation_space`
@@ -23,7 +23,7 @@ def calcQ(state, action):
     Returns:
         v_sum: value for given state, action
     """
-    Vsum = 0
+    v_sum = 0
     transitions = []
     slip_action = env.slip_action_map[action]
     env.set_state(state)
@@ -33,34 +33,34 @@ def calcQ(state, action):
     next_state, reward, _ = env.step(action, slip=False)
     transitions.append((reward, next_state, 1 - env.slip))
     for reward, next_state, pi in transitions:
-        Vsum += pi * (reward + discount * state_values[next_state])
-    return Vsum
+        v_sum += pi * (reward + discount * state_values[next_state])
+    return v_sum
 
 
 # Value Iteration
 for i in range(iters):
-    tmpV = np.zeros(state_dim)
+    v_s = np.zeros(state_dim)
     for state in range(state_dim):
         if env.idx2cell[int(state / 8)] == env.goal_pos:
             continue
-        Vmax = float("-inf")
+        v_max = float("-inf")
         for action in range(env.action_space.n):
-            Vsum = calcQ(state, action)
-            Vmax = max(Vmax, Vsum)
-        tmpV[state] = Vmax
-    state_values = np.copy(tmpV)
+            v_sum = calculate_values(state, action)
+            v_max = max(v_max, v_sum)
+        v_s[state] = v_max
+    state_values = np.copy(v_s)
 
 for state in range(state_dim):
     for action in range(env.action_space.n):
-        Qval[state, action] = calcQ(state, action)
+        q_values[state, action] = calculate_values(state, action)
 
 for state in range(state_dim):
-    policy[state] = np.argmax(Qval[state, :])
+    policy[state] = np.argmax(q_values[state, :])
 
-np.save("Results/Q_Values", Qval)
-print(Qval)
-# np.save('Optimal_policies',optpolicies)
+np.save("q_values", q_values)
+np.save("optimal_policy", policy)
+# print(q_values)
 print("Action mapping:[0 - UP; 1 - DOWN; 2 - LEFT; 3 - RIGHT")
 print("Optimal actions:")
 print(policy)
-value_function_plot(Qval, env)
+value_function_plot(q_values, env)
