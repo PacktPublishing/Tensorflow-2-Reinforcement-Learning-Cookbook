334d6a27d7e4291764a736b5fb114b697609841b
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-07-27 23:47:39 -0700

    Add triangular plots for visualizing action value functions

--- src/ch2-value-based-policy-gradients-and-actor-critic-methods/value_function_utils.py
+++ src/ch2-value-based-policy-gradients-and-actor-critic-methods/value_function_utils.py
@@ -82,3 +82,121 @@ def visualize_grid_state_values(grid_state_values):
         square=True,
     )
     p.set_ylim(len(grid_state_values) + 0.01, -0.01)
+
+
+def visualize_grid_action_values(grid_action_values):
+    top = grid_action_values[:, 0].reshape((3, 4))
+    top_value_positions = [
+        (0.38, 0.25),
+        (1.38, 0.25),
+        (2.38, 0.25),
+        (3.38, 0.25),
+        (0.38, 1.25),
+        (1.38, 1.25),
+        (2.38, 1.25),
+        (3.38, 1.25),
+        (0.38, 2.25),
+        (1.38, 2.25),
+        (2.38, 2.25),
+        (3.38, 2.25),
+    ]
+    right = grid_action_values[:, 1].reshape((3, 4))
+    right_value_positions = [
+        (0.65, 0.5),
+        (1.65, 0.5),
+        (2.65, 0.5),
+        (3.65, 0.5),
+        (0.65, 1.5),
+        (1.65, 1.5),
+        (2.65, 1.5),
+        (3.65, 1.5),
+        (0.65, 2.5),
+        (1.65, 2.5),
+        (2.65, 2.5),
+        (3.65, 2.5),
+    ]
+    bottom = grid_action_values[:, 2].reshape((3, 4))
+    bottom_value_positions = [
+        (0.38, 0.8),
+        (1.38, 0.8),
+        (2.38, 0.8),
+        (3.38, 0.8),
+        (0.38, 1.8),
+        (1.38, 1.8),
+        (2.38, 1.8),
+        (3.38, 1.8),
+        (0.38, 2.8),
+        (1.38, 2.8),
+        (2.38, 2.8),
+        (3.38, 2.8),
+    ]
+    left = grid_action_values[:, 3].reshape((3, 4))
+    left_value_positions = [
+        (0.05, 0.5),
+        (1.05, 0.5),
+        (2.05, 0.5),
+        (3.05, 0.5),
+        (0.05, 1.5),
+        (1.05, 1.5),
+        (2.05, 1.5),
+        (3.05, 1.5),
+        (0.05, 2.5),
+        (1.05, 2.5),
+        (2.05, 2.5),
+        (3.05, 2.5),
+    ]
+
+    fig, ax = plt.subplots(figsize=(12, 5))
+    ax.set_ylim(3, 0)
+    tripcolor = plot_triangular(
+        left,
+        top,
+        right,
+        bottom,
+        ax=ax,
+        triplotkw={"color": "k", "lw": 1},
+        tripcolorkw={"cmap": "coolwarm"},
+    )
+
+    ax.margins(0)
+    ax.set_aspect("equal")
+    fig.colorbar(tripcolor)
+
+    for i, (xi, yi) in enumerate(top_value_positions):
+        plt.text(xi, yi, round(top.flatten()[i], 2), size=11, color="w")
+    for i, (xi, yi) in enumerate(right_value_positions):
+        plt.text(xi, yi, round(right.flatten()[i], 2), size=11, color="w")
+    for i, (xi, yi) in enumerate(left_value_positions):
+        plt.text(xi, yi, round(left.flatten()[i], 2), size=11, color="w")
+    for i, (xi, yi) in enumerate(bottom_value_positions):
+        plt.text(xi, yi, round(bottom.flatten()[i], 2), size=11, color="w")
+
+    plt.show()
+
+
+def plot_triangular(left, bottom, right, top, ax=None, triplotkw={}, tripcolorkw={}):
+
+    if not ax:
+        ax = plt.gca()
+    n = left.shape[0]
+    m = left.shape[1]
+
+    a = np.array([[0, 0], [0, 1], [0.5, 0.5], [1, 0], [1, 1]])
+    tr = np.array([[0, 1, 2], [0, 2, 3], [2, 3, 4], [1, 2, 4]])
+
+    A = np.zeros((n * m * 5, 2))
+    Tr = np.zeros((n * m * 4, 3))
+
+    for i in range(n):
+        for j in range(m):
+            k = i * m + j
+            A[k * 5 : (k + 1) * 5, :] = np.c_[a[:, 0] + j, a[:, 1] + i]
+            Tr[k * 4 : (k + 1) * 4, :] = tr + k * 5
+
+    C = np.c_[
+        left.flatten(), bottom.flatten(), right.flatten(), top.flatten()
+    ].flatten()
+
+    _ = ax.triplot(A[:, 0], A[:, 1], Tr, **triplotkw)
+    tripcolor = ax.tripcolor(A[:, 0], A[:, 1], Tr, facecolors=C, **tripcolorkw)
+    return tripcolor
