1bc1ffea87a5536d6cdc0141318e465a80899dc6
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-07-28 21:13:06 -0700

    Add temporal difference learning

---
+++ src/ch2-value-based-policy-gradients-and-actor-critic-methods/3_temporal_difference_learning.py
@@ -0,0 +1,31 @@
+import numpy as np
+from envs.gridworldv2 import Gridworldv2
+
+
+def temporal_difference_learning(env, max_episodes):
+    grid_state_values = np.zeros(len(env.distinct_states))
+    grid_state_values[env.goal_state] = 1
+    grid_state_values[env.bomb_state] = -1
+    # v: state-value function
+    v = grid_state_values
+    gamma = 0.99  # Discount factor
+    alpha = 0.01  # learning rate
+    done = False
+
+    for episode in range(max_episodes):
+        state = env.reset()
+        while not done:
+            action = np.random.randint(4)  # random policy
+            next_state, reward, done = env.step(action)
+
+            # State-value function updates using TD(0)
+            v[state] += alpha * (reward + gamma * v[next_state] - v[state])
+            state = next_state
+
+
+if __name__ == "__main__":
+    # init the environment:
+    max_episodes = 5000
+    env = Gridworldv2(step_cost=-0.1, max_ep_length=30)
+    temporal_difference_learning(env, max_episodes)
+
