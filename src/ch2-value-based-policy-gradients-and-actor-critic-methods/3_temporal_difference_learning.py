1cdf8531693132623f63e843a8b2c2e8fc4a002a
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-07-29 23:36:02 -0700

    Update env naming and value function visualization

--- src/ch2-value-based-policy-gradients-and-actor-critic-methods/3_temporal_difference_learning.py
+++ src/ch2-value-based-policy-gradients-and-actor-critic-methods/3_temporal_difference_learning.py
@@ -1,9 +1,10 @@
 import numpy as np
-from envs.gridworldv2 import Gridworldv2
+from envs.gridworldv2 import GridworldV2Env
+from value_function_utils import visualize_grid_state_values
 
 
 def temporal_difference_learning(env, max_episodes):
-    grid_state_values = np.zeros(len(env.distinct_states))
+    grid_state_values = np.zeros((len(env.distinct_states), 1))
     grid_state_values[env.goal_state] = 1
     grid_state_values[env.bomb_state] = -1
     # v: state-value function
@@ -21,11 +22,11 @@ def temporal_difference_learning(env, max_episodes):
             # State-value function updates using TD(0)
             v[state] += alpha * (reward + gamma * v[next_state] - v[state])
             state = next_state
+    visualize_grid_state_values(grid_state_values.reshape((3, 4)))
 
 
 if __name__ == "__main__":
-    # init the environment:
     max_episodes = 4000
-    env = Gridworldv2(step_cost=-0.1, max_ep_length=30)
+    env = GridworldV2Env(step_cost=-0.1, max_ep_length=30)
     temporal_difference_learning(env, max_episodes)
 
