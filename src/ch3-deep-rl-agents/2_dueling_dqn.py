a992c842f703a866f480739fc20ff59da2aa1c93
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-08-17 23:39:49 -0700

    Add Dueling DQN Agent

---
+++ src/ch3-deep-rl-agents/2_dueling_dqn.py
@@ -0,0 +1,141 @@
+import argparse
+import os
+import random
+from collections import deque
+from datetime import datetime
+
+import gym
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Add, Dense, Input
+from tensorflow.keras.optimizers import Adam
+
+tf.keras.backend.set_floatx("float64")
+
+parser = argparse.ArgumentParser(prog="TFRL-Cookbook-Ch3-DuelingDQN")
+parser.add_argument("--env", default="CartPole-v0")
+parser.add_argument("--lr", type=float, default=0.005)
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--gamma", type=float, default=0.95)
+parser.add_argument("--eps", type=float, default=1.0)
+parser.add_argument("--eps_decay", type=float, default=0.995)
+parser.add_argument("--eps_min", type=float, default=0.01)
+parser.add_argument("--logdir", default="logs")
+
+args = parser.parse_args()
+logdir = os.path.join(
+    args.logdir, parser.prog, args.env, datetime.now().strftime("%Y%m%d-%H%M%S")
+)
+print(f"Saving training logs to:{logdir}")
+writer = tf.summary.create_file_writer(logdir)
+
+
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def put(self, state, action, reward, next_state, done):
+        self.buffer.append([state, action, reward, next_state, done])
+
+    def sample(self):
+        sample = random.sample(self.buffer, args.batch_size)
+        states, actions, rewards, next_states, done = map(np.asarray, zip(*sample))
+        states = np.array(states).reshape(args.batch_size, -1)
+        next_states = np.array(next_states).reshape(args.batch_size, -1)
+        return states, actions, rewards, next_states, done
+
+    def size(self):
+        return len(self.buffer)
+
+
+class DuelingDQN:
+    def __init__(self, state_dim, aciton_dim):
+        self.state_dim = state_dim
+        self.action_dim = aciton_dim
+        self.epsilon = args.eps
+
+        self.model = self.nn_model()
+
+    def nn_model(self):
+        backbone = tf.keras.Sequential(
+            [
+                Input((self.state_dim,)),
+                Dense(32, activation="relu"),
+                Dense(16, activation="relu"),
+            ]
+        )
+        state_input = Input((self.state_dim,))
+        backbone_1 = Dense(32, activation="relu")(state_input)
+        backbone_2 = Dense(16, activation="relu")(backbone_1)
+        value_output = Dense(1)(backbone_2)
+        advantage_output = Dense(self.action_dim)(backbone_2)
+        output = Add()([value_output, advantage_output])
+        model = tf.keras.Model(state_input, output)
+        model.compile(loss="mse", optimizer=Adam(args.lr))
+        return model
+
+    def predict(self, state):
+        return self.model.predict(state)
+
+    def get_action(self, state):
+        state = np.reshape(state, [1, self.state_dim])
+        self.epsilon *= args.eps_decay
+        self.epsilon = max(self.epsilon, args.eps_min)
+        q_value = self.predict(state)[0]
+        if np.random.random() < self.epsilon:
+            return random.randint(0, self.action_dim - 1)
+        return np.argmax(q_value)
+
+    def train(self, states, targets):
+        self.model.fit(states, targets, epochs=1, verbose=0)
+
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+        self.state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.n
+
+        self.model = DuelingDQN(self.state_dim, self.action_dim)
+        self.target_model = DuelingDQN(self.state_dim, self.action_dim)
+        self.update_target()
+
+        self.buffer = ReplayBuffer()
+
+    def update_target(self):
+        weights = self.model.model.get_weights()
+        self.target_model.model.set_weights(weights)
+
+    def replay_experience(self):
+        for _ in range(10):
+            states, actions, rewards, next_states, done = self.buffer.sample()
+            targets = self.target_model.predict(states)
+            next_q_values = self.target_model.predict(next_states).max(axis=1)
+            targets[range(args.batch_size), actions] = (
+                rewards + (1 - done) * next_q_values * args.gamma
+            )
+            self.model.train(states, targets)
+
+    def train(self, max_episodes=1000):
+        with writer.as_default():
+            for ep in range(max_episodes):
+                done, episode_reward = False, 0
+                state = self.env.reset()
+                while not done:
+                    action = self.model.get_action(state)
+                    next_state, reward, done, _ = self.env.step(action)
+                    self.buffer.put(state, action, reward * 0.01, next_state, done)
+                    episode_reward += reward
+                    state = next_state
+
+                if self.buffer.size() >= args.batch_size:
+                    self.replay_experience()
+                self.update_target()
+                print(f"Episode#{ep} Reward:{episode_reward}")
+                tf.summary.scalar("episode_reward", episode_reward, step=ep)
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    agent = Agent(env)
+    agent.train(max_episodes=1000)
