007d8b08b9ab27d72c4ceee3aa1f17b78a4dd136
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-10-04 21:07:04 -0700

    Add Actor conv net

--- src/ch5-intelligent-web-assistants/1_call_to_action_agent.py
+++ src/ch5-intelligent-web-assistants/1_call_to_action_agent.py
@@ -38,3 +38,42 @@ logdir = os.path.join(
 print(f"Saving training logs to:{logdir}")
 writer = tf.summary.create_file_writer(logdir)
 
+
+class Actor:
+    def __init__(self, state_dim, action_dim, action_bound, std_bound):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_bound = action_bound
+        self.std_bound = std_bound
+        self.model = self.nn_model()
+        self.opt = tf.keras.optimizers.Adam(args.actor_lr)
+
+    def nn_model(self):
+        obs_input = Input(self.state_dim)
+        conv1 = Conv2D(
+            filters=64,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="same",
+            input_shape=self.state_dim,
+            data_format="channels_last",
+            activation="relu",
+        )(obs_input)
+        pool1 = MaxPool2D(pool_size=(3, 3), strides=2)(conv1)
+        conv2 = Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool1)
+        pool2 = MaxPool2D(pool_size=(3, 3), strides=2)(conv2)
+        flat = Flatten(pool2)
+        dense1 = Dense(128, activation="relu")(flat)
+        dropout1 = Dropout(0.3)(dense1)
+        dense2 = Dense(64, activation="relu")(dropout1)
+        dropout2 = Dropout(0.3)(dense2)
+        output_val = Dense(self.action_dim, activation="tanh")(dropout2)
+        mu_output = Lambda(lambda x: x * self.action_bound)(output_val)
+        std_output = Dense(self.action_dim, activation="softplus")(dropout2)
+        return tf.keras.models.Model(obs_input, [mu_output, std_output])
