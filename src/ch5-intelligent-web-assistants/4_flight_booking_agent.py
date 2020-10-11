2601b73fd9a5c6ad6ac07cafc097b491702863c5
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-10-10 18:15:44 -0700

    Update default env arg value

--- src/ch5-intelligent-web-assistants/4_flight_booking_agent.py
+++ src/ch5-intelligent-web-assistants/4_flight_booking_agent.py
@@ -20,7 +20,7 @@ import webgym  # Used to register webgym environments
 tf.keras.backend.set_floatx("float64")
 
 parser = argparse.ArgumentParser(prog="TFRL-Cookbook-Ch5-Flight-Booking-Agent")
-parser.add_argument("--env", default="MiniWoBClickButtonVisualEnv-v0")
+parser.add_argument("--env", default="MiniWoBBookFlightVisualEnv-v0")
 parser.add_argument("--update-freq", type=int, default=16)
 parser.add_argument("--epochs", type=int, default=3)
 parser.add_argument("--actor-lr", type=float, default=1e-4)
