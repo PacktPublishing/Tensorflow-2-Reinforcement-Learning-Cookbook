95fccfa1fdbdea26112f27a9895d8ba7fd314759
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-10-30 19:20:43 -0700

    Register envs with Gym registry

---
+++ src/ch7-cloud-deploy-deep-rl-agents/tradegym/__init__.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+from gym.envs.registration import register
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+_AVAILABLE_ENVS = {
+    "CryptoTradingEnv-v0": {
+        "entry_point": "tradegym.crypto_trading_env:CryptoTradingEnv",
+        "description": "Crypto Trading RL environment",
+    },
+    "StockTradingContinuousEnv-v0": {
+        "entry_point": "tradegym.stock_trading_continuous:StockTradingContinuousEnv",
+        "description": "Stock Trading  RL environment with continous action space",
+    },
+}
+
+
+for env_id, val in _AVAILABLE_ENVS.items():
+    register(id=env_id, entry_point=val.get("entry_point"))
