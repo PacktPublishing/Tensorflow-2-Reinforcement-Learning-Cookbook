0b91962d18e7ee96bdc6ffe2a16c99b55fac7588
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-11-17 19:42:13 -0800

    Add recipe for building-blocks for distributed RL using Ray

---
+++ src/ch9-distributed-training/4_building_blocks_for_distributed_rl_using_ray.py
@@ -0,0 +1,187 @@
+import pickle
+import sys
+
+import fire
+import gym
+import numpy as np
+import ray
+
+if "." not in sys.path:
+    sys.path.insert(0, ".")
+from sac_agent_base import SAC
+
+
+@ray.remote
+class ParameterServer(object):
+    def __init__(self, weights):
+        values = [value.copy() for value in weights]
+        self.weights = values
+
+    def push(self, weights):
+        values = [value.copy() for value in weights]
+        self.weights = values
+
+    def pull(self):
+        return self.weights
+
+    def get_weights(self):
+        return self.weights
+
+    # save weights to disk
+    def save_weights(self, name):
+        with open(name + "weights.pickle", "wb") as pkl:
+            pickle.dump(self.weights, pkl)
+
+
+@ray.remote
+class ReplayBuffer:
+    """
+    A simple FIFO experience replay buffer for RL Agents
+    """
+
+    def __init__(self, obs_shape, action_shape, size):
+        self.cur_states = np.zeros([size, obs_shape[0]], dtype=np.float32)
+        self.actions = np.zeros([size, action_shape[0]], dtype=np.float32)
+        self.rewards = np.zeros(size, dtype=np.float32)
+        self.next_states = np.zeros([size, obs_shape[0]], dtype=np.float32)
+        self.dones = np.zeros(size, dtype=np.float32)
+        self.idx, self.size, self.max_size = 0, 0, size
+        self.rollout_steps = 0
+
+    def store(self, obs, act, rew, next_obs, done):
+        self.cur_states[self.idx] = np.squeeze(obs)
+        self.actions[self.idx] = np.squeeze(act)
+        self.rewards[self.idx] = np.squeeze(rew)
+        self.next_states[self.idx] = np.squeeze(next_obs)
+        self.dones[self.idx] = done
+        self.idx = (self.idx + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+        self.rollout_steps += 1
+
+    def sample_batch(self, batch_size=32):
+        idxs = np.random.randint(0, self.size, size=batch_size)
+        return dict(
+            cur_states=self.cur_states[idxs],
+            actions=self.actions[idxs],
+            rewards=self.rewards[idxs],
+            next_states=self.next_states[idxs],
+            dones=self.dones[idxs],
+        )
+
+    def get_counts(self):
+        return self.rollout_steps
+
+
+@ray.remote
+def rollout(ps, replay_buffer, config):
+    """Collect experience using an exploration policy"""
+    env = gym.make(config["env"])
+    obs, reward, done, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+    total_steps = config["steps_per_epoch"] * config["epochs"]
+
+    agent = SAC(env.observation_space.shape, env.action_space)
+    weights = ray.get(ps.pull.remote())
+    target_weights = agent.actor.get_weights()
+    for i in range(len(target_weights)):  # set tau% of target model to be new weights
+        target_weights[i] = weights[i]
+    agent.actor.set_weights(target_weights)
+
+    for step in range(total_steps):
+        if step > config["random_exploration_steps"]:
+            # Use Agent's policy for exploration after `random_exploration_steps`
+            a = agent.act(obs)
+        else:  # Use a uniform random exploration policy
+            a = env.action_space.sample()
+
+        next_obs, reward, done, _ = env.step(a)
+        ep_ret += reward
+        ep_len += 1
+
+        done = False if ep_len == config["max_ep_len"] else done
+
+        # Store experience to replay buffer
+        replay_buffer.store.remote(obs, a, reward, next_obs, done)
+
+        obs = next_obs
+
+        if done or (ep_len == config["max_ep_len"]):
+            """
+            Perform parameter updates at the end of the trajectory.
+            """
+            obs, reward, done, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+            weights = ray.get(ps.pull.remote())
+            agent.actor.set_weights(weights)
+
+
+@ray.remote(num_gpus=1, max_calls=1)
+def train(ps, replay_buffer, config):
+    agent = SAC(config["obs_shape"], config["action_space"])
+
+    weights = ray.get(ps.pull.remote())
+    agent.actor.set_weights(weights)
+
+    train_step = 1
+    while True:
+
+        agent.train_with_distributed_replay_memory(
+            ray.get(replay_buffer.sample_batch.remote())
+        )
+
+        if train_step % config["worker_update_freq"] == 0:
+            weights = agent.actor.get_weights()
+            ps.push.remote(weights)
+        train_step += 1
+
+
+def main(
+    env="MountainCarContinuous-v0",
+    epochs=1000,
+    steps_per_epoch=5000,
+    replay_size=100000,
+    random_exploration_steps=1000,
+    max_ep_len=1000,
+    num_workers=4,
+    num_learners=1,
+    worker_update_freq=500,
+):
+    config = {
+        "env": env,
+        "epochs": epochs,
+        "steps_per_epoch": steps_per_epoch,
+        "max_ep_len": max_ep_len,
+        "replay_size": replay_size,
+        "random_exploration_steps": random_exploration_steps,
+        "num_workers": num_workers,
+        "num_learners": num_learners,
+        "worker_update_freq": worker_update_freq,
+    }
+
+    env = gym.make(config["env"])
+    config["obs_shape"] = env.observation_space.shape
+    config["action_space"] = env.action_space
+
+    ray.init()
+
+    agent = SAC(config["obs_shape"], config["action_space"])
+    params_server = ParameterServer.remote(agent.actor.get_weights())
+
+    replay_buffer = ReplayBuffer.remote(
+        config["obs_shape"], config["action_space"].shape, config["replay_size"]
+    )
+
+    task_rollout = [
+        rollout.remote(params_server, replay_buffer, config)
+        for i in range(config["num_workers"])
+    ]
+
+    task_train = [
+        train.remote(params_server, replay_buffer, config)
+        for i in range(config["num_learners"])
+    ]
+
+    ray.wait(task_rollout)
+    ray.wait(task_train)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ src/ch9-distributed-training/sac_agent_base.py
@@ -0,0 +1,309 @@
+import functools
+import random
+from collections import deque
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow.keras.layers import Concatenate, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+tf.keras.backend.set_floatx("float64")
+
+
+def actor(state_shape, action_shape, units=(512, 256, 64)):
+    state_shape_flattened = functools.reduce(lambda x, y: x * y, state_shape)
+    state = Input(shape=state_shape_flattened)
+    x = Dense(units[0], name="L0", activation="relu")(state)
+    for index in range(1, len(units)):
+        x = Dense(units[index], name="L{}".format(index), activation="relu")(x)
+
+    actions_mean = Dense(action_shape[0], name="Out_mean")(x)
+    actions_std = Dense(action_shape[0], name="Out_std")(x)
+
+    model = Model(inputs=state, outputs=[actions_mean, actions_std], name="Actor")
+
+    return model
+
+
+def critic(state_shape, action_shape, units=(512, 256, 64)):
+    state_shape_flattened = functools.reduce(lambda x, y: x * y, state_shape)
+    inputs = [Input(shape=state_shape_flattened), Input(shape=action_shape)]
+    concat = Concatenate(axis=-1)(inputs)
+    x = Dense(units[0], name="Hidden0", activation="relu")(concat)
+    for index in range(1, len(units)):
+        x = Dense(units[index], name="Hidden{}".format(index), activation="relu")(x)
+
+    output = Dense(1, name="Out_QVal")(x)
+    model = Model(inputs=inputs, outputs=output, name="Critic")
+
+    return model
+
+
+def update_target_weights(model, target_model, tau=0.005):
+    weights = model.get_weights()
+    target_weights = target_model.get_weights()
+    for i in range(len(target_weights)):  # set tau% of target model to be new weights
+        target_weights[i] = weights[i] * tau + target_weights[i] * (1 - tau)
+    target_model.set_weights(target_weights)
+
+
+class SAC(object):
+    def __init__(
+        self,
+        observation_shape,
+        action_space,
+        lr_actor=3e-5,
+        lr_critic=3e-4,
+        actor_units=(64, 64),
+        critic_units=(64, 64),
+        auto_alpha=True,
+        alpha=0.2,
+        tau=0.005,
+        gamma=0.99,
+        batch_size=128,
+        memory_cap=100000,
+    ):
+        self.state_shape = observation_shape  # shape of observations
+        self.action_shape = action_space.shape  # number of actions
+        self.action_bound = (action_space.high - action_space.low) / 2
+        self.action_shift = (action_space.high + action_space.low) / 2
+        self.memory = deque(maxlen=int(memory_cap))
+
+        # Define and initialize actor network
+        self.actor = actor(self.state_shape, self.action_shape, actor_units)
+        self.actor_optimizer = Adam(learning_rate=lr_actor)
+        self.log_std_min = -20
+        self.log_std_max = 2
+        print(self.actor.summary())
+
+        # Define and initialize critic networks
+        self.critic_1 = critic(self.state_shape, self.action_shape, critic_units)
+        self.critic_target_1 = critic(self.state_shape, self.action_shape, critic_units)
+        self.critic_optimizer_1 = Adam(learning_rate=lr_critic)
+        update_target_weights(self.critic_1, self.critic_target_1, tau=1.0)
+
+        self.critic_2 = critic(self.state_shape, self.action_shape, critic_units)
+        self.critic_target_2 = critic(self.state_shape, self.action_shape, critic_units)
+        self.critic_optimizer_2 = Adam(learning_rate=lr_critic)
+        update_target_weights(self.critic_2, self.critic_target_2, tau=1.0)
+
+        print(self.critic_1.summary())
+
+        # Define and initialize temperature alpha and target entropy
+        self.auto_alpha = auto_alpha
+        if auto_alpha:
+            self.target_entropy = -np.prod(self.action_shape)
+            self.log_alpha = tf.Variable(0.0, dtype=tf.float64)
+            self.alpha = tf.Variable(0.0, dtype=tf.float64)
+            self.alpha.assign(tf.exp(self.log_alpha))
+            self.alpha_optimizer = Adam(learning_rate=lr_actor)
+        else:
+            self.alpha = tf.Variable(alpha, dtype=tf.float64)
+
+        # Set hyperparameters
+        self.gamma = gamma  # discount factor
+        self.tau = tau  # target model update
+        self.batch_size = batch_size
+
+        # Tensorboard
+        self.summaries = {}
+
+    def process_actions(self, mean, log_std, test=False, eps=1e-6):
+        std = tf.math.exp(log_std)
+        raw_actions = mean
+
+        if not test:
+            raw_actions += tf.random.normal(shape=mean.shape, dtype=tf.float64) * std
+
+        log_prob_u = tfp.distributions.Normal(loc=mean, scale=std).log_prob(raw_actions)
+        actions = tf.math.tanh(raw_actions)
+
+        log_prob = tf.reduce_sum(log_prob_u - tf.math.log(1 - actions ** 2 + eps))
+
+        actions = actions * self.action_bound + self.action_shift
+
+        return actions, log_prob
+
+    def act(self, state, test=False, use_random=False):
+        state = state.reshape(-1)  # Flatten state
+        state = np.expand_dims(state, axis=0).astype(np.float64)
+
+        if use_random and len(self.memory) > self.batch_size:
+            a = tf.random.uniform(
+                shape=(1, self.action_shape[0]), minval=-1, maxval=1, dtype=tf.float64
+            )
+        else:
+            means, log_stds = self.actor.predict(state)
+            log_stds = tf.clip_by_value(log_stds, self.log_std_min, self.log_std_max)
+
+            a, log_prob = self.process_actions(means, log_stds, test=test)
+
+        q1 = self.critic_1.predict([state, a])[0][0]
+        q2 = self.critic_2.predict([state, a])[0][0]
+        self.summaries["q_min"] = tf.math.minimum(q1, q2)
+        self.summaries["q_mean"] = np.mean([q1, q2])
+
+        return a
+
+    def save_model(self, a_fn, c_fn):
+        self.actor.save(a_fn)
+        self.critic_1.save(c_fn)
+
+    def load_actor(self, a_fn):
+        self.actor.load_weights(a_fn)
+        print(self.actor.summary())
+
+    def load_critic(self, c_fn):
+        self.critic_1.load_weights(c_fn)
+        self.critic_target_1.load_weights(c_fn)
+        self.critic_2.load_weights(c_fn)
+        self.critic_target_2.load_weights(c_fn)
+        print(self.critic_1.summary())
+
+    def remember(self, state, action, reward, next_state, done):
+        state = state.reshape(-1)  # Flatten state
+        state = np.expand_dims(state, axis=0)
+        next_state = next_state.reshape(-1)  # Flatten next-state
+        next_state = np.expand_dims(next_state, axis=0)
+        self.memory.append([state, action, reward, next_state, done])
+
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+
+        samples = random.sample(self.memory, self.batch_size)
+        s = np.array(samples).T
+        states, actions, rewards, next_states, dones = [
+            np.vstack(s[i, :]).astype(np.float) for i in range(5)
+        ]
+
+        with tf.GradientTape(persistent=True) as tape:
+            # next state action log probs
+            means, log_stds = self.actor(next_states)
+            log_stds = tf.clip_by_value(log_stds, self.log_std_min, self.log_std_max)
+            next_actions, log_probs = self.process_actions(means, log_stds)
+
+            # critics loss
+            current_q_1 = self.critic_1([states, actions])
+            current_q_2 = self.critic_2([states, actions])
+            next_q_1 = self.critic_target_1([next_states, next_actions])
+            next_q_2 = self.critic_target_2([next_states, next_actions])
+            next_q_min = tf.math.minimum(next_q_1, next_q_2)
+            state_values = next_q_min - self.alpha * log_probs
+            target_qs = tf.stop_gradient(
+                rewards + state_values * self.gamma * (1.0 - dones)
+            )
+            critic_loss_1 = tf.reduce_mean(
+                0.5 * tf.math.square(current_q_1 - target_qs)
+            )
+            critic_loss_2 = tf.reduce_mean(
+                0.5 * tf.math.square(current_q_2 - target_qs)
+            )
+
+            # current state action log probs
+            means, log_stds = self.actor(states)
+            log_stds = tf.clip_by_value(log_stds, self.log_std_min, self.log_std_max)
+            actions, log_probs = self.process_actions(means, log_stds)
+
+            # actor loss
+            current_q_1 = self.critic_1([states, actions])
+            current_q_2 = self.critic_2([states, actions])
+            current_q_min = tf.math.minimum(current_q_1, current_q_2)
+            actor_loss = tf.reduce_mean(self.alpha * log_probs - current_q_min)
+
+            # temperature loss
+            if self.auto_alpha:
+                alpha_loss = -tf.reduce_mean(
+                    (self.log_alpha * tf.stop_gradient(log_probs + self.target_entropy))
+                )
+
+        critic_grad = tape.gradient(
+            critic_loss_1, self.critic_1.trainable_variables
+        )  # compute actor gradient
+        self.critic_optimizer_1.apply_gradients(
+            zip(critic_grad, self.critic_1.trainable_variables)
+        )
+
+        critic_grad = tape.gradient(
+            critic_loss_2, self.critic_2.trainable_variables
+        )  # compute actor gradient
+        self.critic_optimizer_2.apply_gradients(
+            zip(critic_grad, self.critic_2.trainable_variables)
+        )
+
+        actor_grad = tape.gradient(
+            actor_loss, self.actor.trainable_variables
+        )  # compute actor gradient
+        self.actor_optimizer.apply_gradients(
+            zip(actor_grad, self.actor.trainable_variables)
+        )
+
+        # tensorboard info
+        self.summaries["q1_loss"] = critic_loss_1
+        self.summaries["q2_loss"] = critic_loss_2
+        self.summaries["actor_loss"] = actor_loss
+
+        if self.auto_alpha:
+            # optimize temperature
+            alpha_grad = tape.gradient(alpha_loss, [self.log_alpha])
+            self.alpha_optimizer.apply_gradients(zip(alpha_grad, [self.log_alpha]))
+            self.alpha.assign(tf.exp(self.log_alpha))
+            # tensorboard info
+            self.summaries["alpha_loss"] = alpha_loss
+
+    def train(self, cur_state, action, reward, next_state, done):
+        self.remember(cur_state, action, reward, next_state, done)  # add to memory
+        self.replay()  # train models through memory replay
+        update_target_weights(
+            self.critic_1, self.critic_target_1, tau=self.tau
+        )  # iterates target model
+        update_target_weights(self.critic_2, self.critic_target_2, tau=self.tau)
+
+    def update_memory(self, xp_store):
+        for (cur_state, action, reward, next_state, done) in zip(
+            xp_store["cur_states"],
+            xp_store["actions"],
+            xp_store["rewards"],
+            xp_store["next_states"],
+            xp_store["dones"],
+        ):
+            self.remember(cur_state, action, reward, next_state, done)  # add to memory
+
+    def train_with_distributed_replay_memory(self, new_experiences):
+        self.update_memory(new_experiences)
+        self.replay()  # train models through memory replay
+        update_target_weights(
+            self.critic_1, self.critic_target_1, tau=self.tau
+        )  # iterates target model
+        update_target_weights(self.critic_2, self.critic_target_2, tau=self.tau)
+
+    def log_status(self, summary_writer, episode_num, reward):
+        """Write training stats using TF `summary_writer`"""
+        with summary_writer.as_default():
+            if len(self.memory) > self.batch_size:
+                tf.summary.scalar(
+                    "Loss/actor_loss", self.summaries["actor_loss"], step=episode_num
+                )
+                tf.summary.scalar(
+                    "Loss/q1_loss", self.summaries["q1_loss"], step=episode_num
+                )
+                tf.summary.scalar(
+                    "Loss/q2_loss", self.summaries["q2_loss"], step=episode_num
+                )
+                if self.auto_alpha:
+                    tf.summary.scalar(
+                        "Loss/alpha_loss",
+                        self.summaries["alpha_loss"],
+                        step=episode_num,
+                    )
+
+            tf.summary.scalar("Stats/alpha", self.alpha, step=episode_num)
+            if self.auto_alpha:
+                tf.summary.scalar("Stats/log_alpha", self.log_alpha, step=episode_num)
+            tf.summary.scalar("Stats/q_min", self.summaries["q_min"], step=episode_num)
+            tf.summary.scalar(
+                "Stats/q_mean", self.summaries["q_mean"], step=episode_num
+            )
+            tf.summary.scalar("Main/step_reward", reward, step=episode_num)
