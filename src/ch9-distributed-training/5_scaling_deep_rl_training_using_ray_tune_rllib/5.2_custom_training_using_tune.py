9926dee9d57472b3f3191d7ef921905f3d4f8471
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-11-20 19:45:53 -0800

    Add quick recipe for custom training loop using IMPALA trainer

---
+++ src/ch9-distributed-training/5_scaling_deep_rl_training_using_ray_tune_rllib/5.2_custom_training_using_tune.py
@@ -0,0 +1,33 @@
+import sys
+
+import ray
+import ray.rllib.agents.impala as impala
+from ray.tune.logger import pretty_print
+from ray.rllib.models import ModelCatalog
+
+if not "." in sys.path:
+    sys.path.insert(0, ".")
+from custom_model import CustomModel
+
+# Register custom-model in ModelCatalog
+ModelCatalog.register_custom_model("CustomCNN", CustomModel)
+
+ray.init()
+config = impala.DEFAULT_CONFIG.copy()
+config["num_gpus"] = 0
+config["num_workers"] = 1
+config["model"]["custom_model"] = "CustomCNN"
+config["log_level"] = "INFO"
+config["framework"] = "tf2"
+trainer = impala.ImpalaTrainer(config=config, env="procgen:procgen-coinrun-v0")
+
+for step in range(1000):
+    # Custom training loop
+    result = trainer.train()
+    print(pretty_print(result))
+
+    if step % 100 == 0:
+        checkpoint = trainer.save()
+        print("checkpoint saved at", checkpoint)
+
+ray.shutdown()
