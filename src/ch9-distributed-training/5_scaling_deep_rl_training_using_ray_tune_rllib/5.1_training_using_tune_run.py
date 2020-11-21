bf653b344e9a77211640be3cb59419ad71301ba8
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-11-20 19:31:03 -0800

    Add a quick recipe to train PPO agent in procgen-coinrun env using tune.run

---
+++ src/ch9-distributed-training/5_scaling_deep_rl_training_using_ray_tune_rllib/5.1_training_using_tune_run.py
@@ -0,0 +1,26 @@
+import ray
+import sys
+from ray import tune
+from ray.rllib.models import ModelCatalog
+
+if not "." in sys.path:
+    sys.path.insert(0, ".")
+from custom_model import CustomModel
+
+# Register custom-model in ModelCatalog
+ModelCatalog.register_custom_model("CustomCNN", CustomModel)
+
+ray.init()
+experiment_analysis = tune.run(
+    "PPO",
+    config={
+        "env": "procgen:procgen-coinrun-v0",
+        "num_gpus": 0,
+        "num_workers": 2,
+        "model": {"custom_model": "CustomCNN"},
+        "framework": "tf2",
+        "log_level": "INFO",
+    },
+    local_dir="ray_results",  # store experiment results in this dir
+)
+ray.shutdown()
