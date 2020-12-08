89431408b6185ae2b7272d5db4c845ffeeaf7258
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-12-07 22:21:04 -0800

    Add DDPG bonus recipe for TFLite export

---
+++ src/ch9/9.z_deep_rl_tflite_deployment/ddpg_agent_tflite_deploy.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env/ python
+# Train & export DDPG agent for building mobile apps using TensorFlow Lite
+# Chapter 9, TensorFlow 2 Reinforcement Learning Cookbook | Praveen Palanisamy
+
+import argparse
+import copy
+import os
+import random
+from collections import deque
+from datetime import datetime
+
+import gym
+import numpy as np
+import tensorflow as tf
+import tensorflowjs as tfjs
+from tensorflow.keras.layers import (
+    Conv2D,
+    Dense,
+    Dropout,
+    Flatten,
+    Input,
+    Lambda,
+    MaxPool2D,
+)
+
+import webgym
+
+tf.keras.backend.set_floatx("float64")
+
+parser = argparse.ArgumentParser(
+    prog="TFRL-Cookbook-Ch9-DDPGAgent-TensorFlowLite-exporter"
+)
+parser.add_argument("--env", default="MiniWoBSocialMediaMuteUserVisualEnv-v0")
+parser.add_argument("--actor_lr", type=float, default=0.0005)
+parser.add_argument("--critic_lr", type=float, default=0.001)
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--tau", type=float, default=0.05)
+parser.add_argument("--gamma", type=float, default=0.99)
+parser.add_argument("--train_start", type=int, default=2000)
+parser.add_argument("--logdir", default="logs")
+
+args = parser.parse_args()
+logdir = os.path.join(
+    args.logdir, parser.prog, args.env, datetime.now().strftime("%Y%m%d-%H%M%S")
+)
+print(f"Saving training logs to:{logdir}")
+writer = tf.summary.create_file_writer(logdir)
+
+
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def store(self, state, action, reward, next_state, done):
+        self.buffer.append([state, action, reward, next_state, done])
+
+    def sample(self):
+        sample = random.sample(self.buffer, args.batch_size)
+        states, actions, rewards, next_states, done = map(np.asarray, zip(*sample))
+        states = np.array(states).reshape(args.batch_size, -1)
+        next_states = np.array(next_states).reshape(args.batch_size, -1)
+        return states, actions, rewards, next_states, done
+
+    def size(self):
+        return len(self.buffer)
+
+
+class Actor:
+    def __init__(self, state_dim, action_dim, action_bound):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_bound = action_bound
+        self.weight_initializer = tf.keras.initializers.he_normal()
+        self.eps = 1e-5
+        self.model = self.nn_model()
+        self.opt = tf.keras.optimizers.Adam(args.actor_lr)
+
+    def nn_model(self):
+        obs_input = Input(self.state_dim)
+        conv1 = Conv2D(
+            filters=64,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="same",
+            input_shape=self.state_dim,
+            data_format="channels_last",
+            activation="relu",
+        )(obs_input)
+        pool1 = MaxPool2D(pool_size=(3, 3), strides=1)(conv1)
+        conv2 = Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool1)
+        pool2 = MaxPool2D(pool_size=(3, 3), strides=1)(conv2)
+        conv3 = Conv2D(
+            filters=16,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool2)
+        pool3 = MaxPool2D(pool_size=(3, 3), strides=1)(conv3)
+        conv4 = Conv2D(
+            filters=8,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool3)
+        pool4 = MaxPool2D(pool_size=(3, 3), strides=1)(conv4)
+        flat = Flatten()(pool4)
+        dense1 = Dense(
+            16, activation="relu", kernel_initializer=self.weight_initializer
+        )(flat)
+        dropout1 = Dropout(0.3)(dense1)
+        dense2 = Dense(
+            8, activation="relu", kernel_initializer=self.weight_initializer
+        )(dropout1)
+        dropout2 = Dropout(0.3)(dense2)
+        # action_dim[0] = 2
+        output_val = Dense(
+            self.action_dim[0],
+            activation="relu",
+            kernel_initializer=self.weight_initializer,
+        )(dropout2)
+        # Scale & clip x[i] to be in range [0, action_bound[i]]
+        action_bound = copy.deepcopy(self.action_bound)
+        mu_output = Lambda(
+            lambda x: tf.clip_by_value(x * action_bound, 1e-9, action_bound),
+            name="mu_output",
+        )(output_val)
+        return tf.keras.models.Model(inputs=obs_input, outputs=mu_output, name="Actor")
+
+    def train(self, states, q_grads):
+        with tf.GradientTape() as tape:
+            grads = tape.gradient(
+                self.model(states), self.model.trainable_variables, -q_grads
+            )
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+
+    def predict(self, state):
+        return self.model.predict(state)
+
+    def get_action(self, state):
+        # Convert [Image] to np.array(np.adarray)
+        state_np = np.array([np.array(s) for s in state])
+        if len(state_np.shape) == 3:
+            # Convert (w, h, c) to (1, w, h, c)
+            state_np = np.expand_dims(state_np, 0)
+        action = self.model.predict(state_np)
+        # Clip action to be between 0 and max obs screen size
+        action = np.clip(action, 0, self.action_bound)
+        # 1 Action per instance of env; Env expects: (num_instances, actions)
+        return action
+
+    def save_tflite(self, model_dir: str, version: int = 1):
+        """Save/Export Actor model in TensorFlow Lite format"""
+        actor_model_save_dir = os.path.join(model_dir, "actor", str(version))
+        model_converter = tf.lite.TFLiteConverter.from_keras_model(self.model)
+        # Convert model to TFLite Flatbuffer
+        tflite_model = model_converter.convert()
+        # Save the model to disk/persistent-storage
+        if not os.path.exists(actor_model_save_dir):
+            os.makedirs(actor_model_save_dir)
+        actor_model_file_name = os.path.join(actor_model_save_dir, "model.tflite")
+        with open(actor_model_file_name, "wb") as model_file:
+            model_file.write(tflite_model)
+        print(f"Actor model saved in TFLite format at:{actor_model_file_name}")
+
+
+class Critic:
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.weight_initializer = tf.keras.initializers.he_normal()
+        self.model = self.nn_model()
+        self.opt = tf.keras.optimizers.Adam(args.critic_lr)
+
+    def nn_model(self):
+        obs_input = Input(self.state_dim)
+        conv1 = Conv2D(
+            filters=64,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="same",
+            input_shape=self.state_dim,
+            data_format="channels_last",
+            activation="relu",
+        )(obs_input)
+        pool1 = MaxPool2D(pool_size=(3, 3), strides=2)(conv1)
+        conv2 = Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool1)
+        pool2 = MaxPool2D(pool_size=(3, 3), strides=2)(conv2)
+        conv3 = Conv2D(
+            filters=16,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool2)
+        pool3 = MaxPool2D(pool_size=(3, 3), strides=1)(conv3)
+        conv4 = Conv2D(
+            filters=8,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+            activation="relu",
+        )(pool3)
+        pool4 = MaxPool2D(pool_size=(3, 3), strides=1)(conv4)
+        flat = Flatten()(pool4)
+        dense1 = Dense(
+            16, activation="relu", kernel_initializer=self.weight_initializer
+        )(flat)
+        dropout1 = Dropout(0.3)(dense1)
+        dense2 = Dense(
+            8, activation="relu", kernel_initializer=self.weight_initializer
+        )(dropout1)
+        dropout2 = Dropout(0.3)(dense2)
+        value = Dense(
+            1, activation="linear", kernel_initializer=self.weight_initializer
+        )(dropout2)
+
+        return tf.keras.models.Model(inputs=obs_input, outputs=value, name="Critic")
+
+    def predict(self, inputs):
+        return self.model.predict(inputs)
+
+    def q_gradients(self, states, actions):
+        actions = tf.convert_to_tensor(actions)
+        with tf.GradientTape() as tape:
+            tape.watch(actions)
+            q_values = self.model([states, actions])
+            q_values = tf.squeeze(q_values)
+        return tape.gradient(q_values, actions)
+
+    def compute_loss(self, v_pred, td_targets):
+        mse = tf.keras.losses.MeanSquaredError()
+        return mse(td_targets, v_pred)
+
+    def train(self, states, actions, td_targets):
+        with tf.GradientTape() as tape:
+            v_pred = self.model([states, actions], training=True)
+            assert v_pred.shape == td_targets.shape
+            loss = self.compute_loss(v_pred, tf.stop_gradient(td_targets))
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+        return loss
+
+    def save_tflite(self, model_dir: str, version: int = 1):
+        """Save/Export Critic model in TensorFlow Lite format"""
+        critic_model_save_dir = os.path.join(model_dir, "critic", str(version))
+        model_converter = tf.lite.TFLiteConverter.from_keras_model(self.model)
+        # Convert model to TFLite Flatbuffer
+        tflite_model = model_converter.convert()
+        # Save the model to disk/persistent-storage
+        if not os.path.exists(critic_model_save_dir):
+            os.makedirs(critic_model_save_dir)
+        critic_model_file_name = os.path.join(critic_model_save_dir, "model.tflite")
+        with open(critic_model_file_name, "wb") as model_file:
+            model_file.write(tflite_model)
+        print(f"Critic model saved in TFLite format at:{critic_model_file_name}")
+
+
+class DDPGAgent:
+    def __init__(self, env):
+        self.env = env
+        self.state_dim = self.env.observation_space.shape
+        self.action_dim = self.env.action_space.shape
+        self.action_bound = self.env.action_space.high
+
+        self.buffer = ReplayBuffer()
+
+        self.actor = Actor(self.state_dim, self.action_dim, self.action_bound)
+        self.critic = Critic(self.state_dim, self.action_dim)
+
+        self.target_actor = Actor(self.state_dim, self.action_dim, self.action_bound)
+        self.target_critic = Critic(self.state_dim, self.action_dim)
+
+        actor_weights = self.actor.model.get_weights()
+        critic_weights = self.critic.model.get_weights()
+        self.target_actor.model.set_weights(actor_weights)
+        self.target_critic.model.set_weights(critic_weights)
+
+    def update_target(self):
+        actor_weights = self.actor.model.get_weights()
+        t_actor_weights = self.target_actor.model.get_weights()
+        critic_weights = self.critic.model.get_weights()
+        t_critic_weights = self.target_critic.model.get_weights()
+
+        for i in range(len(actor_weights)):
+            t_actor_weights[i] = (
+                args.tau * actor_weights[i] + (1 - args.tau) * t_actor_weights[i]
+            )
+
+        for i in range(len(critic_weights)):
+            t_critic_weights[i] = (
+                args.tau * critic_weights[i] + (1 - args.tau) * t_critic_weights[i]
+            )
+
+        self.target_actor.model.set_weights(t_actor_weights)
+        self.target_critic.model.set_weights(t_critic_weights)
+
+    def get_td_target(self, rewards, q_values, dones):
+        targets = np.asarray(q_values)
+        for i in range(q_values.shape[0]):
+            if dones[i]:
+                targets[i] = rewards[i]
+            else:
+                targets[i] = args.gamma * q_values[i]
+        return targets
+
+    def add_ou_noise(self, x, rho=0.15, mu=0, dt=1e-1, sigma=0.2, dim=1):
+        return (
+            x + rho * (mu - x) * dt + sigma * np.sqrt(dt) * np.random.normal(size=dim)
+        )
+
+    def replay_experience(self):
+        for _ in range(10):
+            states, actions, rewards, next_states, dones = self.buffer.sample()
+            target_q_values = self.target_critic.predict(
+                [next_states, self.target_actor.predict(next_states)]
+            )
+            td_targets = self.get_td_target(rewards, target_q_values, dones)
+
+            self.critic.train(states, actions, td_targets)
+
+            s_actions = self.actor.predict(states)
+            s_grads = self.critic.q_gradients(states, s_actions)
+            grads = np.array(s_grads).reshape((-1, self.action_dim))
+            self.actor.train(states, grads)
+            self.update_target()
+
+    def train(self, max_episodes=1000):
+        with writer.as_default():
+            for ep in range(max_episodes):
+                step_num, episode_reward, done = 0, 0, False
+
+                state = self.env.reset()
+                prev_state = state
+                bg_noise = np.random.randint(
+                    self.env.action_space.low,
+                    self.env.action_space.high,
+                    self.env.action_space.shape,
+                )
+                while not done:
+                    # self.env.render()
+                    action = self.actor.get_action(state)
+                    noise = self.add_ou_noise(bg_noise, dim=self.action_dim)
+                    action = np.clip(action + noise, 0, self.action_bound).astype("int")
+
+                    next_state, reward, dones, _ = self.env.step(action)
+                    done = np.all(dones)
+                    if done:
+                        next_state = prev_state
+                    else:
+                        prev_state = next_state
+
+                    for (s, a, r, s_n, d) in zip(
+                        next_state, action, reward, next_state, dones
+                    ):
+                        self.buffer.store(s, a, (r + 8) / 8, s_n, d)
+                        episode_reward += r
+
+                    step_num += 1  # 1 across num_instances
+                    print(
+                        f"ep#:{ep} step#:{step_num} step_rew:{reward} action:{action} dones:{dones}"
+                    )
+
+                    bg_noise = noise
+                    state = next_state
+                if (
+                    self.buffer.size() >= args.batch_size
+                    and self.buffer.size() >= args.train_start
+                ):
+                    self.replay_experience()
+                print(f"Episode#{ep} Reward:{episode_reward}")
+                tf.summary.scalar("episode_reward", episode_reward, step=ep)
+
+    def save_tflite(self, model_dir: str, version: int = 1):
+        # Make sure `toco_from_protos binary` is on system's PATH to avoid TFLite ConverterError
+        toco_bin_dir = os.path.dirname(sys.executable)
+        if not toco_bin_dir in os.environ["PATH"]:
+            os.environ["PATH"] += os.pathsep + toco_bin_dir
+        print(f"Saving Agent model (TFLite) to:{model_dir}\n")
+        self.actor.save_tflite(model_dir, version)
+        self.critic.save_tflite(model_dir, version)
+
+
+if __name__ == "__main__":
+    env_name = args.env
+    env = gym.make(env_name)
+    agent = DDPGAgent(env)
+    agent.train(max_episodes=1)
+    # Model saving
+    model_dir = "trained_models"
+    agent_name = f"DDPG_{env_name}"
+    agent_version = 1
+    agent_model_path = os.path.join(model_dir, agent_name)
+    agent.save_tflite(agent_model_path, agent_version)
